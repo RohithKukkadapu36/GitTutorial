0ca4ecb248e95fb159776ac890afe6a0d6ca7cd3
Rohith <rohithkukkadapu.400@gmail.com>
2024-05-25 13:51:24 +0530

    I have made changes to see the conflicts

--- src/com/git/Test3.java
+++ src/com/git/Test3.java
@@ -4,7 +4,7 @@ public class Test3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("I will push to other branch");
+		System.out.println("I will push to main branch for conflict check");
 	}
 
 }
