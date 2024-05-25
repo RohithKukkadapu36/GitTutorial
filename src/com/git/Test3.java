fb665cfee0e4d3e06494dd0dac442a8790b988ac
Rohith <rohithkukkadapu.400@gmail.com>
2024-05-25 13:54:48 +0530

    added integer values

--- src/com/git/Test3.java
+++ src/com/git/Test3.java
@@ -4,6 +4,8 @@ public class Test3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int x=36;
+		System.out.println("X value is: "+x);
 		System.out.println("I will push to main branch for conflict check");
 	}
 
